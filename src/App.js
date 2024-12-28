4e719a9543a6863cce5450ed47f3aed2c90429cd
TheJoaoVitorio <joaovtr999x@gmail.com>
2024-12-28 20:37:20 -0300

    update css header menu

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <img src='https://m.media-amazon.com/images/G/32/digital/video/Magellan_MLP/BRND_MTH24_PVMagnetLeft_1440x675_POST_Final_pt-BR_GSS00019491.jpg' ></img>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,57 +6,64 @@ import { IoIosArrowDown } from "react-icons/io";
 
 export default function Header(){
     return(
-        <header className={styles.header} >
-            <div className={styles.headerContent} >
-                <div className={styles.headerContentLeft} >
-                    <div className={styles.logoHeader} >
-                        <img src="https://m.media-amazon.com/images/G/01/digital/video/web/logo-min-remaster.png" alt="logo" ></img>
+        <header className={styles.header}>
+            <div className={styles.headerContent}>
+                <div className={styles.headerContentLeft}>
+                    <div className={styles.logoHeader}>
+                        <img src="https://m.media-amazon.com/images/G/01/digital/video/web/logo-min-remaster.png" alt="logo" />
                     </div>
 
-                    <div className={styles.headerItensContainer} >
-                        <ul>
-                            <div className={styles.headerItemMenuDrop} >
-                                <li>
+                    <div className={styles.headerItensContainer}>
+                        <ul id={styles.headerListItens}>
+                            <div className={styles.headerItemMenuDrop}>
+                                <li className={styles.menuTrigger}>
                                     Menu
-                                    <IoIosArrowDown
-                                        size={20}
-                                    />
+                                    <IoIosArrowDown size={20} />
+                                    <div className={styles.dropdownContent}>
+                                        <ul>
+                                            <li>Home</li>
+                                            <li>Movies</li>
+                                            <li>TV Shows</li>
+                                            <li>Sports</li>
+                                            <li>Live TV</li>
+                                        </ul>
+                                    </div>
                                 </li>
                             </div>
-                            <div className={styles.headerItens} >
+                            <div className={styles.headerItens}>
                                 <li>Home</li>
                                 <li>Movies</li>
                                 <li>TV Shows</li>
                                 <li>Sports</li>
                                 <li>Live TV</li>
                             </div>
-                            
                         </ul>
                     </div>
-                </div> 
+                </div>
 
-                <div className={styles.headerContentRight} > 
-                    <div className={styles.changeLanguageButton} >
+                <div className={styles.headerContentRight}>
+                    <div className={styles.changeLanguageButton}>
                         <div className={styles.changeLanguageButtonContent}>
                             EN
-                            <img src={arrowDown} ></img>
+                            <img src={arrowDown} alt="language selector" />
                         </div>
                     </div>
                 
-                    <div className={styles.headerLogin} >
-                        <div className={styles.avatarContainer} >
+                    <div className={styles.headerLogin}>
+                        <div className={styles.avatarContainer}>
                             <div>
-                                <img src={menuIco}></img>
+                                <img src={menuIco} alt="menu" />
                             </div>
                             <a>
                                 <img
-                                    className={styles.avatarLogin} 
-                                    src={avatarSvg} >
-                                </img>
+                                    className={styles.avatarLogin}
+                                    src={avatarSvg}
+                                    alt="avatar"
+                                />
                             </a>
                         </div>
                         
-                        <button className={styles.buttonLogin} >
+                        <button className={styles.buttonLogin}>
                             Join Prime
                         </button>
                     </div>
