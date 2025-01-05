e622b7b64a7715c2c0c36c6511e1840a931a1c74
TheJoaoVitorio <joaovtr999x@gmail.com>
2025-01-04 23:02:28 -0300

    create routes, menuCategories

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <img src='https://m.media-amazon.com/images/G/32/digital/video/Magellan_MLP/BRND_MTH24_PVMagnetLeft_1440x675_POST_Final_pt-BR_GSS00019491.jpg' ></img>
+        <Header/>
+        {/* <img src='https://m.media-amazon.com/images/G/32/digital/video/Magellan_MLP/BRND_MTH24_PVMagnetLeft_1440x675_POST_Final_pt-BR_GSS00019491.jpg' ></img> */}
     </div>
   );
 }
